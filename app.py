33e9805c3b18c984f15480f20180453bb8f97e4e
Jonas Diete <jonas.diete@gmail.com>
2021-11-03 13:30:37 +0000

    playable version

--- app.py
+++ app.py
@@ -5,19 +5,6 @@ app = Flask(__name__)
 # Key for signing the cookies
 app.secret_key = "SANDY_CANYON_SUNSET"
 
-# Global Variables
-# timestable that was selected
-tt = 0
-# username that logged in
-username = ""
-
-user_medals_str = ""
-
-# Message displayed on login page
-login_message = "Enter your login details."
-# Message displayed on register page
-register_message = "Register"
-
 @app.route("/")
 def index():
     return redirect("/login")
@@ -40,23 +27,13 @@ def login():
             # Saving username in session cookie
             session["username"] = username_entered
 
-            # Saving username in global variable
-            #global username
-            #username = username_entered
-
-            # Changing message, ready for next login
-            global login_message
-            login_message = "Enter your login details."
             return redirect("/select")
         else:
             return render_template("login.html", login_message = "Incorrect username or password. Try again.")
     else:    
-        global register_message
-        register_message = "Register"
         if "username" in session:
             session.pop("username", default=None) # Deleting username in case we were redirected here after logout
-        #username = ""   # In case we were redirected here after logout
-        return render_template("login.html", login_message = login_message)
+        return render_template("login.html", login_message = "Enter your login details.")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -72,73 +49,63 @@ def register():
             for row in userreader:
                 if row:     # This is checking if the row is "true", i.e. not empty
                     users[row[0]] = row[1]
-        if not new_username in users:
-            # Checking if username is alphanumeric:
-            if new_username.isalnum():
-                # Checking if username has at least 2 characters
-                if len(new_username) > 1:
-                    # Checking if passwords match
-                    if new_pw1 == new_pw2:
-                        # Checking there are no spaces in password
-                        if not (" " in new_pw1):
-                            # Checking if password has at least 4 characters
-                            if len(new_pw1) > 3:
-                                    # Saving new username and password in CSV file
-                                    with open("data/users.csv", "a", newline="") as usernamefile:
-                                        userwriter = csv.writer(usernamefile, delimiter=" ")
-                                        userwriter.writerow([new_username, new_pw1])
-                                    
-                                    # Adding entry in medals CSV file for the new user
-                                    # 0 = no medals, 1 = bronze, 2 = silver, 3 = gold
-                                    # Arranged in order of timestables, starting with 2x
-                                    with open("data/medals.csv", "a", newline="") as medalsfile:
-                                        medalswriter = csv.writer(medalsfile, delimiter=" ")
-                                        medalswriter.writerow([new_username, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-                                    
-                                    global login_message 
-                                    login_message = "New user created. Please login."
-                                    return redirect("/login")
-                            else:
-                                global register_message
-                                register_message = "Password needs to be at least 4 characters long. Try again."
-                                return redirect("/register")
-                        else:
-                            register_message = "No spaces allowed in password. Try again."
-                            return redirect("/register")
-                    else:
-                        register_message = "Passwords don't match. Try again."
-                        return redirect("/register")
-                else:
-                    register_message = "Please enter a longer username."
-                    return redirect("/register")
-            else: 
-                register_message = "Only letters or numbers allowed for username. Try again."
-                return redirect("/register")
+        if new_username in users:
+            return render_template("register.html", register_message = "Username exists already. Try again.")
+        
+        # Checking if username is alphanumeric
+        elif not new_username.isalnum():
+            return render_template("register.html", register_message = "Only letters or numbers allowed for username. Try again.")
+        
+        # Checking if username has at least 2 characters
+        elif len(new_username) < 2:
+            return render_template("register.html", register_message = "Please enter a longer username.")
+
+        # Checking if passwords match
+        elif new_pw1 != new_pw2:
+            return render_template("register.html", register_message = "Passwords don't match. Try again.")
+        
+        # Checking there are no spaces in password
+        elif " " in new_pw1:
+            return render_template("register.html", register_message = "No spaces allowed in password. Try again.")
+        
+        # Checking new password has at least 4 characters
+        elif len(new_pw1) < 4:
+            return render_template("register.html", register_message = "Password must be at least 4 characters long. Try again.")
+        
         else:
-            register_message = "Username exists already. Try again."
-            return redirect("/register")
+            # Saving new username and password in CSV file
+            with open("data/users.csv", "a", newline="") as usernamefile:
+                userwriter = csv.writer(usernamefile, delimiter=" ")
+                userwriter.writerow([new_username, new_pw1])
+                                    
+            # Adding entry in medals CSV file for the new user
+            # 0 = no medals, 1 = bronze, 2 = silver, 3 = gold
+            # Arranged in order of timestables, starting with 2x
+            with open("data/medals.csv", "a", newline="") as medalsfile:
+                medalswriter = csv.writer(medalsfile, delimiter=" ")
+                medalswriter.writerow([new_username, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) 
+
+            return render_template("login.html", login_message = "New user created. Please Login.")              
+        
     else:
-        login_message = "Enter your login details."
-        return render_template("register.html", register_message = register_message)
+        return render_template("register.html", register_message = "Register a new user.")
 
 @app.route("/select", methods=["GET", "POST"])
 def select():
     
     if not "username" in session:
-    #if username == "":
         return redirect("/login")
     else:
         if request.method == "POST":
                 
             # Saving the timestable the user has selected
-            global tt
-            tt = int(request.form.get("timestable"))
+            tt_selected = request.form.get("timestable")
 
             # Checking if "test" or "practise" has been clicked
             if request.form.get("test_practise") == "test":
-                return redirect("/test")
+                return redirect("/test/" + tt_selected)
             else:
-                return redirect("/practise")
+                return redirect("/practise/" + tt_selected)
         
         else:
             # Getting medal-data and saving it in user_medals
@@ -146,8 +113,6 @@ def select():
                 medalsreader = csv.reader(medalsfile, delimiter=" ")
                 for row in medalsreader:
                     if row[0] == session["username"]:
-                    #if row[0] == username:
-                        #global user_medals_str
                         user_medals_str = ""
                         for i in range(11):
                             user_medals_str += (row[i + 1])
@@ -156,15 +121,14 @@ def select():
             return render_template("select.html", username = session["username"], tts = range(3, 13), medals = user_medals_str)
     
 
-@app.route("/test", methods=["GET", "POST"])
-def test():
+@app.route("/test/<tt>", methods=["GET", "POST"])
+def test(tt):
     if not "username" in session:
-    #if username == "":
         return redirect("/login")
     else:
         if request.method == "GET":
             return render_template("test.html", timestable = tt, username = session["username"])
-        else:
+        else:  # Data was sent through POST
             # Receiving which medal was won, when timestable test was completed
             medal_earned = request.form.get("medal_earned")
             
@@ -179,7 +143,7 @@ def test():
                 # Updating all_medals with the medal earned from current user and current timestable
                 for row in all_medals:
                     if row[0] == session["username"]:
-                        row[tt - 1] = medal_earned
+                        row[int(tt) - 1] = medal_earned
 
                 # Overwriting content of CSV file with updated all_medals
                 with open("data/medals.csv", "w", newline="") as medalsfile:
@@ -187,13 +151,12 @@ def test():
                     for row in all_medals:
                         medalswriter.writerow(row)
 
-            return redirect("/select")
+            return redirect("/select") # Doesn't work! Getting back with back button instead.
 
 
-@app.route("/practise", methods=["GET", "POST"])
-def practise():
+@app.route("/practise/<tt>", methods=["GET", "POST"])
+def practise(tt):
     if not "username" in session:
-    #if username == "":
         return redirect("/login")
     else:
         return render_template("practise.html", timestable = tt, username = session["username"])
