508e643b9a6a76b4d584d7cd66aaa3d8285417aa
Jonas Diete <jonas.diete@gmail.com>
2023-05-05 08:48:31 +0100

    testing accept box hides cookies

--- test_my_application.py
+++ test_my_application.py
@@ -2,20 +2,14 @@ import re
 from playwright.sync_api import Page, expect
 
 
-def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_intro_page(page: Page):
+def test_homepage_has_correct_title(page: Page):
     page.goto("http://127.0.0.1:5000")
 
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Times Table Coach"))
 
-    # # create a locator
-    # get_started = page.get_by_role("link", name="Get started")
-
-    # # Expect an attribute "to be strictly equal" to the value.
-    # expect(get_started).to_have_attribute("href", "/docs/intro")
-
-    # # Click the get started link.
-    # get_started.click()
-
-    # # Expects the URL to contain intro.
-    # expect(page).to_have_url(re.compile(".*intro"))
+def test_accepting_cookies_hides_box(page: Page):
+    page.goto("http://127.0.0.1:5000")
+    accept_button = page.get_by_role("button", name="Accept").click()
+    # checking accept_button is None (doesn't exist)
+    assert not accept_button
