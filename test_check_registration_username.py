8ea7f8c1d442675e6f9a0dd93c9507f805becfda
Jonas Diete <jonas.diete@gmail.com>
2023-05-06 11:34:11 +0100

    testing too short username returns error

--- test_check_registration_username.py
+++ test_check_registration_username.py
@@ -8,3 +8,11 @@ def test_valid_usernames_return_none():
     assert check_registration_username("ROLERT183") == None
     assert check_registration_username("1531") == None
     assert check_registration_username("Little13Mice") == None
+    assert check_registration_username("Pi") == None
+
+def test_too_short_usernames_return_correct_message():
+    message = "Please enter a longer username."
+    assert check_registration_username("L") == message
+    assert check_registration_username("a") == message
+    assert check_registration_username("9") == message
+    assert check_registration_username("0") == message
