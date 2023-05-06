4d9e28043290e63b188d7b58c35783386dc19e72
Jonas Diete <jonas.diete@gmail.com>
2023-05-06 12:23:45 +0100

    added more tests to check passwords are matching

--- test_check_registration_password.py
+++ test_check_registration_password.py
@@ -3,3 +3,6 @@ from app import check_registration_password
 def test_non_matching_passwords_return_correct_error_message():
     message = "Passwords don't match. Try again."
     assert check_registration_password("password", "pasword") == message
+    assert check_registration_password("Tester", "tester") == message
+    assert check_registration_password("Pass_word", "Password") == message
+    assert check_registration_password("PASS", "pass") == message
