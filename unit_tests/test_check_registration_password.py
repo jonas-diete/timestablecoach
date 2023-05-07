34efa5be9b9525ea7b0f9de356e1d77d7f15242b
Jonas Diete <jonas.diete@gmail.com>
2023-05-07 07:43:50 +0100

    tested password for minimum length

--- unit_tests/test_check_registration_password.py
+++ unit_tests/test_check_registration_password.py
@@ -13,3 +13,10 @@ def test_spaces_in_password_return_correct_error_message():
     assert check_registration_password("Tester 1", "Tester 1") == message
     assert check_registration_password("  Password", "  Password") == message
     assert check_registration_password("pass word ", "pass word ") == message
+
+def test_password_at_least_4_chars():
+    message = "Password must be at least 4 characters long. Try again."
+    assert check_registration_password("hi", "hi") == message
+    assert check_registration_password("", "") == message
+    assert check_registration_password("pas", "pas") == message
+    assert check_registration_password("1", "1") == message
