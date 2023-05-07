f54a5af53bf8ce8c6f26125ebf490b92c78e19c1
Jonas Diete <jonas.diete@gmail.com>
2023-05-07 07:41:11 +0100

    tested for spaces in password

--- unit_tests/test_check_registration_password.py
+++ unit_tests/test_check_registration_password.py
@@ -6,3 +6,10 @@ def test_non_matching_passwords_return_correct_error_message():
     assert check_registration_password("Tester", "tester") == message
     assert check_registration_password("Pass_word", "Password") == message
     assert check_registration_password("PASS", "pass") == message
+
+def test_spaces_in_password_return_correct_error_message():
+    message = "No spaces allowed in password. Try again."
+    assert check_registration_password("pass word", "pass word") == message
+    assert check_registration_password("Tester 1", "Tester 1") == message
+    assert check_registration_password("  Password", "  Password") == message
+    assert check_registration_password("pass word ", "pass word ") == message
