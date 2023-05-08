4bba9fff9feb1477350148bc30255f47d48fe5d3
Jonas Diete <jonas.diete@gmail.com>
2023-05-08 10:00:25 +0100

    added spaces for code legibility

--- app.py
+++ app.py
@@ -118,13 +118,17 @@ def login():
         # saving user input
         username_entered = request.form.get("username")
         password_entered = request.form.get("password")
+
         # checking for error message on username and password
         # this ensures no special characters etc are used to hack database
         if check_username(username_entered) == None and check_password(password_entered) == None:
+            
             # trying to load user from database
             user_retrieved = user_repository.get_one(database_connection.connect(), username_entered) 
+            
             # checking user exists and password matches database
             if user_retrieved and bcrypt.checkpw(password_entered.encode('utf-8'), user_retrieved.password.encode('utf-8')):
+                
                 # everything is correct - logging in
                 session["user"] = jsonpickle.encode(user_retrieved)
                 return redirect("/select")
@@ -132,7 +136,7 @@ def login():
         # username didn't exist or password was wrong
         return render_template("login.html", login_message = "Incorrect username or password. Try again.", cookies = "yes")
         
-        
+
     elif request.method == 'GET':   
         # deleting user in case we were redirected here after logout
         if "user" in session:
