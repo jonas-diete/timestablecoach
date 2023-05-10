161cb2b84b7ca70a7261c9c53bc957d759f8620f
Jonas Diete <jonas.diete@gmail.com>
2023-05-10 15:52:30 +0100

    corrected indentation on timestable class

--- lib/timestable.py
+++ lib/timestable.py
@@ -1,10 +1,10 @@
 class Timestable:
-  def __init__(self, name, factors_learned, gold=False, silver=False, bronze=False, id=0):
-    self.id = id
-    self.name = name
-    self.gold = gold
-    self.silver = silver
-    self.bronze = bronze
-    # factors_learned will be filled like this
-    # {1: FactorLearned(), 2: FactorLearned(), 3: FactorLearned(), ...}
-    self.factors_learned = factors_learned
+    def __init__(self, name, factors_learned, gold=False, silver=False, bronze=False, id=0):
+        self.id = id
+        self.name = name
+        self.gold = gold
+        self.silver = silver
+        self.bronze = bronze
+        # factors_learned will be filled like this
+        # {1: FactorLearned(), 2: FactorLearned(), 3: FactorLearned(), ...}
+        self.factors_learned = factors_learned
